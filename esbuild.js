147f529bc47990c154dee060942a3277903c6645
Stan van Rooy <me@stanvanrooy.com>
2024-07-08 15:27:01 +0200

    feat: implement build as executable

---
+++ esbuild.js
@@ -0,0 +1,38 @@
+import esbuild from 'esbuild'
+
+// Custom plugin to handle the # prefixed modules
+const resolveVendorModulesPlugin = {
+    name: 'resolve-vendor-modules',
+    setup(build) {
+        build.onResolve({ filter: /^#.*$/ }, args => {
+            if (args.path === '#ansi-styles') {
+              const relativePath = "node_modules/chalk/source/vendor/ansi-styles/index.js";
+              const cwd = process.cwd();
+              return {
+                path: `${cwd}/${relativePath}`,
+              };
+            } else if (args.path === '#supports-color') {
+              const relativePath = "node_modules/chalk/source/vendor/supports-color/index.js";
+              const cwd = process.cwd();
+              return {
+                path: `${cwd}/${relativePath}`,
+              };
+            }
+        });
+    }
+};
+
+esbuild.build({
+    entryPoints: ['src/index.ts'],
+    outdir: 'dist',
+    bundle: true,
+    sourcemap: true,
+    target: 'node20',
+    platform: 'node',
+    format: 'esm',
+    plugins: [resolveVendorModulesPlugin],
+})
+.then(async (result) => {
+    console.log('Build complete');
+})
+.catch(() => process.exit(1));
